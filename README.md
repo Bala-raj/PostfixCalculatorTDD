97bdd1add5d1a5caa4711f202e69984732a67dcb
James Mortensen <james.mortensen@a-cti.com>
2015-10-14 20:17:54 +0530

    Modified system so gradle manages all dependencies and classpath using gradle eclipse. But it doesn't integrate nicely with Eclipse and requires repeat user hacks.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PostfixCalculatorTDD
+
+## Configure system and environment variables for the Eclipse Gradle App Engine plugin
+
+Run the following commands with the path to your App Engine SDK installation:
+
+```
+$ launchctl setenv APPENGINE_HOME /usr/local/appengine/appengine-java-sdk-1.9.26
+
+$ launchctl setenv appengine.sdk.root /usr/local/appengine/appengine-java-sdk-1.9.26
+```
--- src/com/full/postfix/Postfix_Calculator_TDDServlet.java
+++ src/com/full/postfix/Postfix_Calculator_TDDServlet.java
@@ -1,12 +1,40 @@
 package com.full.postfix;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.full.pojo.Employee;
 
 @SuppressWarnings("serial")
 public class Postfix_Calculator_TDDServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		String employeeString = serializeEmployee();
+		resp.getWriter().println("Hello, world. Here's the employee: " + employeeString);
+	}
+
+	private String serializeEmployee() {
+		Employee employee = new Employee(1, "Lokesh", "Gupta", new Date(1981, 8, 18));
+		ObjectMapper mapper = new ObjectMapper();
+		String employeeString = "";
+		try {
+			//mapper.defaultPrettyPrintingWriter().writeValue(new File("c://temp/employee.json"), employee);
+			employeeString = mapper.writeValueAsString(employee);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return employeeString;
 	}
 }
